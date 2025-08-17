1ef7c3630135b4d9a1cc356dc88020eb573961e7
kojix2 <2xijok@gmail.com>
2025-08-18 00:26:53 +0900

    experimental: Add uiImageView

--- unix/uipriv_unix.h
+++ unix/uipriv_unix.h
@@ -49,6 +49,7 @@ extern void uiprivFreeContext(uiDrawContext *);
 
 // image.c
 extern cairo_surface_t *uiprivImageAppropriateSurface(uiImage *i, GtkWidget *w);
+extern cairo_surface_t *uiprivImageCopyAppropriateSurface(uiImage *i, GtkWidget *w);
 
 // cellrendererbutton.c
 extern GtkCellRenderer *uiprivNewCellRendererButton(void);
