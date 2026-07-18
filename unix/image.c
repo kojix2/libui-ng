138686e92ad4844e453c3ebb645d1d68f9b2de12
kojix2 <2xijok@gmail.com>
2026-07-18 17:04:55 +0900

    unix: avoid stride overflow warning in image append

--- unix/image.c
+++ unix/image.c
@@ -64,6 +64,7 @@ void uiImageAppend(uiImage *i, void *pixels, int pixelWidth, int pixelHeight, in
 {
 	cairo_surface_t *cs;
 	uint8_t *data, *pix;
+	int64_t minStride;
 	int realStride;
 	int x, y;
 
@@ -77,7 +78,8 @@ void uiImageAppend(uiImage *i, void *pixels, int pixelWidth, int pixelHeight, in
 		uiprivUserBug("You cannot append a uiImage representation with pixel height %d.", pixelHeight);
 	if (pixelWidth > INT_MAX / 4)
 		uiprivUserBug("You cannot append a uiImage representation with pixel width %d.", pixelWidth);
-	if (byteStride < pixelWidth * 4)
+	minStride = (int64_t) pixelWidth * 4;
+	if (byteStride < minStride)
 		uiprivUserBug("You cannot append a uiImage representation with byte stride %d and pixel width %d.", byteStride, pixelWidth);
 
 	// note that this is native-endian
