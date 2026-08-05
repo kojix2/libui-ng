24eb2191e4e5424166ff417449d31f6b6ce4b83d
kojix2 <2xijok@gmail.com>
2026-08-05 16:10:17 +0900

    unix: replace DateTimePicker popup with GtkPopover

--- unix/datetimepicker.c
+++ unix/datetimepicker.c
@@ -11,14 +11,12 @@ typedef struct uiprivDateTimePickerWidget uiprivDateTimePickerWidget;
 typedef struct uiprivDateTimePickerWidgetClass uiprivDateTimePickerWidgetClass;
 
 struct uiprivDateTimePickerWidget {
-	GtkToggleButton parent_instance;
-
-	gulong toggledSignal;
+	GtkMenuButton parent_instance;
 
 	gboolean hasTime;
 	gboolean hasDate;
 
-	GtkWidget *window;
+	GtkWidget *popover;
 	GtkWidget *box;
 	GtkWidget *calendar;
 	GtkWidget *timebox;
@@ -32,16 +30,13 @@ struct uiprivDateTimePickerWidget {
 	gulong minutesBlock;
 	gulong secondsBlock;
 	gulong ampmBlock;
-
-	GdkDevice *keyboard;
-	GdkDevice *mouse;
 };
 
 struct uiprivDateTimePickerWidgetClass {
-	GtkToggleButtonClass parent_class;
+	GtkMenuButtonClass parent_class;
 };
 
-G_DEFINE_TYPE(uiprivDateTimePickerWidget, uiprivDateTimePickerWidget, GTK_TYPE_TOGGLE_BUTTON)
+G_DEFINE_TYPE(uiprivDateTimePickerWidget, uiprivDateTimePickerWidget, GTK_TYPE_MENU_BUTTON)
 
 static int realSpinValue(GtkSpinButton *spinButton)
 {
@@ -113,199 +108,6 @@ static void dateTimeChanged(uiprivDateTimePickerWidget *d)
 	g_signal_emit(d, changedSignal, 0);
 	setLabel(d);
 	// TODO fire event here instead?
-}
-
-// we don't want ::toggled to be sent again
-static void setActive(uiprivDateTimePickerWidget *d, gboolean active)
-{
-	g_signal_handler_block(d, d->toggledSignal);
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d), active);
-	g_signal_handler_unblock(d, d->toggledSignal);
-}
-
-// like startGrab() below, a lot of this is in the order that GtkComboBox does it
-static void endGrab(uiprivDateTimePickerWidget *d)
-{
-	// This can be called during teardown even when no popup grab is active.
-	if (d->keyboard != NULL)
-		gdk_device_ungrab(d->keyboard, GDK_CURRENT_TIME);
-	if (d->mouse != NULL) {
-		gdk_device_ungrab(d->mouse, GDK_CURRENT_TIME);
-		gtk_device_grab_remove(d->window, d->mouse);
-	}
-	d->keyboard = NULL;
-	d->mouse = NULL;
-}
-
-static void hidePopup(uiprivDateTimePickerWidget *d)
-{
-	endGrab(d);
-	gtk_widget_hide(d->window);
-	setActive(d, FALSE);
-}
-
-// this consolidates a good chunk of what GtkComboBox does
-static gboolean startGrab(uiprivDateTimePickerWidget *d)
-{
-	GdkDevice *dev;
-	guint32 time;
-	GdkWindow *window;
-	GdkDevice *keyboard, *mouse;
-
-	dev = gtk_get_current_event_device();
-	if (dev == NULL) {
-		// this is what GtkComboBox does
-		// since no device was set, just use the first available "master device"
-		GdkDisplay *disp;
-		GdkDeviceManager *dm;
-		GList *list;
-
-		disp = gtk_widget_get_display(GTK_WIDGET(d));
-		dm = gdk_display_get_device_manager(disp);
-		list = gdk_device_manager_list_devices(dm, GDK_DEVICE_TYPE_MASTER);
-		dev = (GdkDevice *) (list->data);
-		g_list_free(list);
-	}
-
-	time = gtk_get_current_event_time();
-	keyboard = dev;
-	mouse = gdk_device_get_associated_device(dev);
-	if (gdk_device_get_source(dev) != GDK_SOURCE_KEYBOARD) {
-		dev = mouse;
-		mouse = keyboard;
-		keyboard = dev;
-	}
-
-	window = gtk_widget_get_window(d->window);
-	if (keyboard != NULL)
-		if (gdk_device_grab(keyboard, window,
-			GDK_OWNERSHIP_WINDOW, TRUE,
-			GDK_KEY_PRESS_MASK | GDK_KEY_RELEASE_MASK,
-			NULL, time) != GDK_GRAB_SUCCESS)
-			return FALSE;
-	if (mouse != NULL)
-		if (gdk_device_grab(mouse, window,
-			GDK_OWNERSHIP_WINDOW, TRUE,
-			GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK | GDK_POINTER_MOTION_MASK,
-			NULL, time) != GDK_GRAB_SUCCESS) {
-			if (keyboard != NULL)
-				gdk_device_ungrab(keyboard, time);
-			return FALSE;
-		}
-
-	if (mouse != NULL)
-		gtk_device_grab_add(d->window, mouse, TRUE);
-	d->keyboard = keyboard;
-	d->mouse = mouse;
-	return TRUE;
-}
-
-// based on gtk_combo_box_list_position() in the GTK+ source code
-static void allocationToScreen(uiprivDateTimePickerWidget *d, gint *x, gint *y)
-{
-	GdkWindow *window;
-	GtkAllocation a;
-	GtkRequisition aWin;
-	GdkScreen *screen;
-	GdkRectangle workarea;
-	int otherY;
-
-	gtk_widget_get_allocation(GTK_WIDGET(d), &a);
-	gtk_widget_get_preferred_size(d->window, &aWin, NULL);
-	*x = 0;
-	*y = 0;
-	if (!gtk_widget_get_has_window(GTK_WIDGET(d))) {
-		*x = a.x;
-		*y = a.y;
-	}
-	window = gtk_widget_get_window(GTK_WIDGET(d));
-	gdk_window_get_root_coords(window, *x, *y, x, y);
-	if (gtk_widget_get_direction(GTK_WIDGET(d)) == GTK_TEXT_DIR_RTL)
-		*x += a.width - aWin.width;
-
-	// now adjust to prevent the box from going offscreen
-	screen = gtk_widget_get_screen(GTK_WIDGET(d));
-	gdk_screen_get_monitor_workarea(screen,
-		gdk_screen_get_monitor_at_window(screen, window),
-		&workarea);
-	if (*x < workarea.x)					// too far to the left?
-		*x = workarea.x;
-	else if (*x + aWin.width > (workarea.x + workarea.width))	// too far to the right?
-		*x = (workarea.x + workarea.width) - aWin.width;
-	// this isn't the same algorithm used by GtkComboBox
-	// first, get our two choices; *y for down and otherY for up
-	otherY = *y - aWin.height;
-	*y += a.height;
-	// and use otherY if we're too low
-	if (*y + aWin.height >= workarea.y + workarea.height)
-		*y = otherY;
-}
-
-static void showPopup(uiprivDateTimePickerWidget *d)
-{
-	GtkWidget *toplevel;
-	gint x, y;
-
-	// GtkComboBox does it
-	toplevel = gtk_widget_get_toplevel(GTK_WIDGET(d));
-	if (GTK_IS_WINDOW(toplevel))
-		gtk_window_group_add_window(gtk_window_get_group(GTK_WINDOW(toplevel)), GTK_WINDOW(d->window));
-
-	allocationToScreen(d, &x, &y);
-	gtk_window_move(GTK_WINDOW(d->window), x, y);
-
-	gtk_widget_show(d->window);
-	setActive(d, TRUE);
-
-	if (!startGrab(d))
-		hidePopup(d);
-}
-
-static void onToggled(GtkToggleButton *b, gpointer data)
-{
-	uiprivDateTimePickerWidget *d = uiprivDateTimePickerWidget(b);
-
-	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(d)))
-		showPopup(d);
-	else
-		hidePopup(d);
-}
-
-static gboolean grabBroken(GtkWidget *w, GdkEventGrabBroken *e, gpointer data)
-{
-	uiprivDateTimePickerWidget *d = uiprivDateTimePickerWidget(data);
-
-	hidePopup(d);
-	return TRUE;		// this is what GtkComboBox does
-}
-
-static gboolean buttonReleased(GtkWidget *w, GdkEventButton *e, gpointer data)
-{
-	uiprivDateTimePickerWidget *d = uiprivDateTimePickerWidget(data);
-	int winx, winy;
-	GtkAllocation wina;
-	gboolean in;
-
-	gtk_widget_get_allocation(d->window, &wina);
-	winx = 0;
-	winy = 0;
-	if (!gtk_widget_get_has_window(d->window)) {
-		winx = wina.x;
-		winy = wina.y;
-	}
-	gdk_window_get_root_coords(gtk_widget_get_window(d->window), winx, winy, &winx, &winy);
-	in = TRUE;
-	if (e->x_root < winx)
-		in = FALSE;
-	if (e->x_root >= (winx + wina.width))
-		in = FALSE;
-	if (e->y_root < winy)
-		in = FALSE;
-	if (e->y_root >= (winy + wina.height))
-		in = FALSE;
-	if (!in)
-		hidePopup(d);
-	return TRUE;		// this is what GtkComboBox does
 }
 
 static gint hoursSpinboxInput(GtkSpinButton *sb, gpointer ptr, gpointer data)
@@ -456,20 +258,13 @@ static void uiprivDateTimePickerWidget_setTime(uiprivDateTimePickerWidget *d, GD
 
 static void uiprivDateTimePickerWidget_init(uiprivDateTimePickerWidget *d)
 {
-	d->window = gtk_window_new(GTK_WINDOW_POPUP);
-	gtk_window_set_resizable(GTK_WINDOW(d->window), FALSE);
-	gtk_window_set_attached_to(GTK_WINDOW(d->window), GTK_WIDGET(d));
-	gtk_window_set_decorated(GTK_WINDOW(d->window), FALSE);
-	gtk_window_set_deletable(GTK_WINDOW(d->window), FALSE);
-	gtk_window_set_type_hint(GTK_WINDOW(d->window), GDK_WINDOW_TYPE_HINT_COMBO);
-	gtk_window_set_skip_taskbar_hint(GTK_WINDOW(d->window), TRUE);
-	gtk_window_set_skip_pager_hint(GTK_WINDOW(d->window), TRUE);
-	gtk_container_set_border_width(GTK_CONTAINER(d->window), 12);
-	// and make it stand out a bit
-	gtk_style_context_add_class(gtk_widget_get_style_context(d->window), "frame");
+	d->popover = gtk_popover_new(GTK_WIDGET(d));
+	gtk_popover_set_position(GTK_POPOVER(d->popover), GTK_POS_BOTTOM);
+	gtk_menu_button_set_popover(GTK_MENU_BUTTON(d), d->popover);
+	gtk_container_set_border_width(GTK_CONTAINER(d->popover), 12);
 
 	d->box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
-	gtk_container_add(GTK_CONTAINER(d->window), d->box);
+	gtk_container_add(GTK_CONTAINER(d->popover), d->box);
 
 	d->calendar = gtk_calendar_new();
 	d->calendarBlock = g_signal_connect(d->calendar, "day-selected", G_CALLBACK(dateChanged), d);
@@ -494,20 +289,12 @@ static void uiprivDateTimePickerWidget_init(uiprivDateTimePickerWidget *d)
 	d->seconds = newSpinbox(d, 0, 59, NULL, zeroPadSpinbox, &(d->secondsBlock));
 	gtk_container_add(GTK_CONTAINER(d->timebox), d->seconds);
 
-	// TODO replace the custom popup and grabs with GtkPopover
 	d->ampm = newSpinbox(d, 0, 1, ampmSpinboxInput, ampmSpinboxOutput, &(d->ampmBlock));
 	gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(d->ampm), FALSE);
 	gtk_widget_set_valign(d->ampm, GTK_ALIGN_CENTER);
 	gtk_container_add(GTK_CONTAINER(d->timebox), d->ampm);
 
 	gtk_widget_show_all(d->box);
-
-	g_signal_connect(d->window, "grab-broken-event", G_CALLBACK(grabBroken), d);
-	g_signal_connect(d->window, "button-release-event", G_CALLBACK(buttonReleased), d);
-
-	d->toggledSignal = g_signal_connect(d, "toggled", G_CALLBACK(onToggled), NULL);
-	d->keyboard = NULL;
-	d->mouse = NULL;
 
 	d->hasTime = TRUE;
 	d->hasDate = TRUE;
@@ -516,27 +303,8 @@ static void uiprivDateTimePickerWidget_init(uiprivDateTimePickerWidget *d)
 	uiprivDateTimePickerWidget_setTime(d, g_date_time_new_now_local());
 }
 
-static void uiprivDateTimePickerWidget_dispose(GObject *obj)
-{
-	uiprivDateTimePickerWidget *d = uiprivDateTimePickerWidget(obj);
-
-	if (d->window != NULL) {
-		gtk_widget_destroy(d->window);
-		d->window = NULL;
-	}
-	G_OBJECT_CLASS(uiprivDateTimePickerWidget_parent_class)->dispose(obj);
-}
-
-static void uiprivDateTimePickerWidget_finalize(GObject *obj)
-{
-	G_OBJECT_CLASS(uiprivDateTimePickerWidget_parent_class)->finalize(obj);
-}
-
 static void uiprivDateTimePickerWidget_class_init(uiprivDateTimePickerWidgetClass *class)
 {
-	G_OBJECT_CLASS(class)->dispose = uiprivDateTimePickerWidget_dispose;
-	G_OBJECT_CLASS(class)->finalize = uiprivDateTimePickerWidget_finalize;
-
 	changedSignal = g_signal_new("changed",
 		G_TYPE_FROM_CLASS(class),
 		G_SIGNAL_RUN_LAST,
@@ -561,7 +329,6 @@ static void uiDateTimePickerDestroy(uiControl *c)
 {
 	uiDateTimePicker *d = uiDateTimePicker(c);
 
-	hidePopup(d->d);
 	if (d->setBlock != 0) {
 		g_signal_handler_disconnect(d->d, d->setBlock);
 		d->setBlock = 0;
