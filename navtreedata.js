6053345596eb30849969dfda3093262b799d1168
kojix2 <kojix2@users.noreply.github.com>
2026-08-22 08:19:11 +0000

    deploy: 90befc67c3487530a6b7a2054a721a1577bdc073

---
+++ navtreedata.js
@@ -0,0 +1,96 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "libui-ng", "index.html", [
+    [ "Branches", "index.html#autotoc_md1", null ],
+    [ "Runtime Requirements", "index.html#autotoc_md2", null ],
+    [ "Build Requirements", "index.html#autotoc_md3", null ],
+    [ "Build", "index.html#autotoc_md4", null ],
+    [ "Test", "index.html#autotoc_md5", null ],
+    [ "Install", "index.html#autotoc_md6", null ],
+    [ "Documentation and Examples", "index.html#autotoc_md7", null ],
+    [ "Contributing", "index.html#autotoc_md8", null ],
+    [ "API", "md_doc_doxygen_doxygen_subpage_api.html", null ],
+    [ "Changelog", "md__c_h_a_n_g_e_l_o_g.html", [
+      [ "[Unreleased]", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md11", [
+        [ "Changed", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md12", null ],
+        [ "Added", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md13", null ],
+        [ "Removed", "md__c_h_a_n_g_e_l_o_g.html#autotoc_md14", null ]
+      ] ]
+    ] ],
+    [ "Contributing to libui-ng", "md__c_o_n_t_r_i_b_u_t_i_n_g.html", [
+      [ "Rules for contributing code", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md16", [
+        [ "Commit messages and pull request description", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md17", null ],
+        [ "Code formatting", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md18", null ],
+        [ "Naming", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md19", null ],
+        [ "API documentation", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md20", null ],
+        [ "Other commenting", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md21", null ],
+        [ "Compatibility", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md22", null ],
+        [ "Windows-specific notes", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md23", null ],
+        [ "GTK+-specific notes", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md24", null ],
+        [ "OS X-specific notes", "md__c_o_n_t_r_i_b_u_t_i_n_g.html#autotoc_md25", null ]
+      ] ]
+    ] ],
+    [ "Old News", "md_doc_old_news.html", [
+      [ "Status", "md_doc_old_news.html#autotoc_md27", null ],
+      [ "News", "md_doc_old_news.html#autotoc_md28", null ]
+    ] ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"structui_font_button.html#a67a4bc81966fc6309a010253910a830e",
+"ui_8h.html#a116b6ce9f3f208d3681dfdac2afd4e3d",
+"ui_8h.html#acd9adceabd66b2738a058e0dbb273110"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ structui_grid.js
@@ -0,0 +1,9 @@
+var structui_grid =
+[
+    [ "uiGridAppend", "structui_grid.html#a612307b31ab131f582ef7c326b4f2580", null ],
+    [ "uiGridDelete", "structui_grid.html#a81fbad04bb77355bbb66dc5959d15ee0", null ],
+    [ "uiGridInsertAt", "structui_grid.html#ad282fc62adbaed067699f949d619899c", null ],
+    [ "uiGridPadded", "structui_grid.html#a59e9bad0b023013e412009305f062f6d", null ],
+    [ "uiGridSetPadded", "structui_grid.html#a3b880352f5b698ad0579a41f020bd725", null ],
+    [ "uiNewGrid", "structui_grid.html#aae419b63d154eaea37d0d06e9e96dce2", null ]
+];
