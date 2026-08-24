dcef8242602cf014f3073267e14561d63638cdcc
kojix2 <2xijok@gmail.com>
2026-08-24 21:46:02 +0900

    image: add destination-aware drawing and image view

--- test/unit/main.c
+++ test/unit/main.c
@@ -88,9 +88,11 @@ int main(void)
 		{ imageRunUnitTests },
 		{ gridRunUnitTests },
 		{ drawMatrixRunUnitTests },
+		{ drawImageRunUnitTests },
 		{ attrstrRunUnitTests },
 		{ tableRunUnitTests },
 		{ toolbarRunUnitTests },
+		{ imageViewRunUnitTests },
 	};
 
 	for (i = 0; i < sizeof(unitTests)/sizeof(*unitTests); ++i) {
