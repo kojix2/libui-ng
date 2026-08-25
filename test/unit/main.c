209075b223717d1485e0cbcda66235d35b298615
kojix2 <2xijok@gmail.com>
2026-08-25 18:54:31 +0900

    Add toolbar

--- test/unit/main.c
+++ test/unit/main.c
@@ -90,6 +90,7 @@ int main(void)
 		{ drawMatrixRunUnitTests },
 		{ attrstrRunUnitTests },
 		{ tableRunUnitTests },
+		{ toolbarRunUnitTests },
 	};
 
 	for (i = 0; i < sizeof(unitTests)/sizeof(*unitTests); ++i) {
